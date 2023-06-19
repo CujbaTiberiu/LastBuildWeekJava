2e17afd004360877641244bcbaaaf61b753b723c
Domenico232 <123988959+Domenico232@users.noreply.github.com>
2023-06-19 18:53:20 +0200

    ciao

--- epicode.energy.service/src/main/java/com/buildweek/epicode/energy/payload/LoginDto.java
+++ epicode.energy.service/src/main/java/com/buildweek/epicode/energy/payload/LoginDto.java
@@ -1,13 +1,13 @@
 package com.buildweek.epicode.energy.payload;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
-@Setter
-@Getter
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
